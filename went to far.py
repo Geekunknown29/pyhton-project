55bb7db1d012f862b7c3999919592c07dc010f5a
Geekunknown29 <123154889+Geekunknown29@users.noreply.github.com>
2024-11-24 16:19:44 +0530

    Add files via upload

--- went to far.py
+++ went to far.py
@@ -1,140 +1,120 @@
-# DATA and patent MANAGEMENT
-print ("R or r for new work , U or u for insertion of data in previous titles")
-a= input("what do you want to do :")
-EMPLIST  = []
+print("R or r for new work, U or u for insertion of data in previous titles")
+a = input("What do you want to do: ")
+EMPLIST = []
+
 def re():
-    desname= input("give a name to the file : ")
+    desname = input("Give a name to the file: ")
     global f
-    f= open (desname,'w')
-    return (f)   
-def th():
-    theorybox = input("write your theory further here:")
-    EMPLIST.append(theorybox)
+    f = open(desname, 'w')
+    return f
+
+def thb():
+    th = input("Write your theory further here: ")
+    EMPLIST.append(th)
+
 def num():
-    mathbox =  "mathematical relation used or talked about in the above part : " + input("input your mathematical expression")
-    EMPLIST.append(mathbox)
+    mat = "Mathematical relation used or talked about in the above part: " + input("Input your mathematical expression: ")
+    EMPLIST.append(mat)
+def extra(nt):
+    if nt == "1":
+        thb()
+    elif nt == "2":
+        num()
+    elif nt == "3":
+        im = input("Provide the address to any image or video you want to show in your work here: ")
+        EMPLIST.append(im)
+    elif nt == "4":
+        biblio = input("Bibliography: ")
+        print('Report has ended, thanks for working with us.')
+        EMPLIST.append('Report ended')
+        EMPLIST.append(biblio)
+
 def nxt():
-    nt = input(" 1 for providing text box for theory , 2 for providing numerical box , 3 for image box ,4 for ending the report:")
-    while nt< '4':
-        if nt == "1":
-            th()
-        elif nt == "2":
-            num()
-        elif nt == '3':
-            imagebox = input("you can give the address to any image or video you want to shaow in your work here : ")
-        nt = input(" 1 for providing text box for theory , 2 for providing numerical box , 3 for image box ,4 for ending the report:")
-       if nt == "4":
-        	biblio=input("bibliography :")
-        	print('report has ended, thanks for working with us')
-        	EMPLIST.append('report ended')
-        	EMPLIST.append(biblio)
+    while True:
+        nt = input("1 for entering any theory, 2 for numerical relation, 3 for entering image link, 4 to end the report: ")
+        if nt in ["1", "2", "3", "4"]:
+            extra(nt)
+            if nt == "4":
+                break 
+        else:
+            print("Invalid input, try again.")
+
 def start():
-    t = input("Title/name of your project:")
-    T = 'TITLE : ' + t
+    t = input("Title/name of your project: ")
+    T = 'TITLE: ' + t
     EMPLIST.append(T)
     print("p for physics, c for chemistry, b for biology, m for mathematics, cs for computer science")
     global s
-    s =  input("Subject under which it does come:")
-    S = 'CODE OF CONCERNED SUBJECT : ' + s
+    s = input("Subject under which it falls: ")
+    S = 'CODE OF CONCERNED SUBJECT: ' + s
     EMPLIST.append(S)
-    return (S)
+
 def password():
-        inp2= int(input("input a password for your work, it will come in handy in future if you would like to   edit any thing in your work : "))
-    istring = str(inp2)
+    
+    inp2 = int(input("Input a password for your work (numerical only): "))
     global rpassword
-    rpassword = 'PASSWORD : ' + istring
+    rpassword = 'PASSWORD: ' + str(inp2)
     return rpassword
 
-if a == 'R' or a == 'r':
+if a.lower() == 'r':
     start()
-    yourname = 'author(s)' + input('name of people who worked in this project : ') + ':'
+    yourname = 'Author(s): ' + input('Name of people who worked on this project: ') + ':'
     EMPLIST.append(yourname)
     re()
     password()
-    EMPLIST.append(rpassword)    
+    EMPLIST.append(rpassword)
 
+    q = "ACCOMPLISHMENT MADE BY THE WORK: " + input("Question/problem solved by your study or accomplishment made: ")
+    im = "image : " +  input("Provide the address to any image or video you want to show in your work here: ")
+    idb = "introduction : " +  "INTRODUCTION: " + input("Provide your theory's introduction: ")
+    fm = m0box = "FINAL MATHEMATICAL RELATION FOUND (if any): " + input("Provide any final mathematical relation or derivation your study suggests, if any: ")
+    th = "theory : " + input("Write part 1 of details of your theory: ")
+    mat = "MATHEMATICAL RELATION USED OR TALKED ABOUT IN ABOVE PART: " + input("Provide any mathematical expression for part 1 of your theory: ")
 
-    if s == 'p' or s == 'P' or s == 'c' or s == 'C' or s == 'm' or s == 'M':
-            q = "ACCOMPLISHMENT MADE BY THE WORK: " + input("question/problem solved by your study or accomplishment made : ")
-            imagebox = input("you can give the address to any image or video you want to shaow in your work here : ")
-            ideabox = "INTRODUCTION: " + input("your theory's introduction: ")
-            m0box = "FINAL MATHEMATICAL RELATION FOUND (if any) : " + input("you can write here if  there is any final matematical relation or derivation which your study suggests ,here  :")
-            theorybox = input("write part 1 of details of your theory, you will get a mathbox below for writing any mathematical relation related to this part of thepory :")
-            mathbox = "MATHEMATIAL RELATION USED OR TALKED ABOUT IN ABOVE PART : " +input("write any mathematical expression for part 1 of your theory:")
-            EMPLIST.append(q)
-            EMPLIST.append(imagebox)
-            EMPLIST.append(ideabox)
-            EMPLIST.append(m0box)  
-            EMPLIST.append(theorybox)
-            EMPLIST.append(mathbox)            
-            nxt()            
+    EMPLIST.append(q)
+    EMPLIST.append(im)
+    EMPLIST.append(idb)
+    EMPLIST.append(fm)
+    EMPLIST.append(th)
+    EMPLIST.append(mat)
 
-    elif s == 'b' or s == 'B':
-            q = "ACCOMPLISHMENT MADE BY THE WORK: " + input("accomplishment made by the work : ")            
-            ideabox = "INTRODUCTION: " + input("your theory's introduction:")
-            imagebox = input("you can give the address to any image you want to shaow in your work here : ")
-            m0box = "mathematical relation which can be concluded from this study : " + input("you can write here the any mathematical relation which your work suggests, if any :")
-            theorybox = input("write part 1 of details of your theory:")
-            mathbox =  "mathematical relation used or talked about in the above part : " + input("write any mathematical expression for part 1 of your theory:")
-            EMPLIST.append(q)
-            EMPLIST.append(ideabox)
-            EMPLIST.append(imagebox)
-            EMPLIST.append(m0box)  
-            EMPLIST.append(theorybox)
-            EMPLIST.append(mathbox)            
-            nxt()
-
-    elif s == 'cs' or s == 'CS' or s == 'Cs' or s == 'cS':
-            q = "ACCOMPLISHMENT MADE BY THE WORK: " + input("accomplishment made by your study : ")
-            imagebox = input("you can give the address to any image you want to shaow in your work here : ")
-            ideabox = "INTRODUCTION: " + input("your work's introduction:")
-            m0box = "MATHEMATICAL RELATION(s) USED HERE ARE: " + input("you can write here the any mathematical relation which you have used in your work, if any :")
-            theorybox = input("you will further get a mathbox for providing mathematical relation used in this part of theory . write part 1 of details of your theory:")
-            mathbox = input("write any mathematical expression for part 1 of your theory:")
-            EMPLIST.append(q)
-            EMPLIST.append(imagebox)
-            EMPLIST.append(ideabox)
-            EMPLIST.append(m0box)  
-            EMPLIST.append(theorybox)
-            EMPLIST.append(mathbox)            
-            nxt()        
-    string1= '\n'.join(EMPLIST)            
-    f.writelines(string1)          
+    nxt()
+    string1 = '\n'.join(EMPLIST)
+    f.writelines(string1)
     f.close()
 
-
-if a == 'u' or a == 'U':
-    EMPLIST.append("Updated File :")
+elif a.lower() == 'u':
     e = input("Name of the file you want to edit: ")
-    passcode = int(input("password : "))
-    passcode1= str(passcode)
-    filere = open(e, 'r')    
-    lines = filere.readlines()
-    p = "PASSWORD : " + passcode1
-    string2= '\n'.join(lines)
-    if p in string2:        
-        print(''.join(lines))
-        linenum = int(input('The Serial no. of line you want to update: '))
-        k = linenum - 1
+    passcode = int(input("Password: "))
+    passcode1 = str(passcode)
+    try:
+        with open(e, 'r') as filere:
+            lines = filere.readlines()
+        p = "PASSWORD: " + passcode1
+        if p in ''.join(lines):
+            print("content of desired file is : ")
+            print(''.join(lines))
+            linenum = int(input('The Serial no. of line you want to update: '))
+            if linenum <= len(lines):
+                lines[linenum - 1] = input('The new text you want at desired line: ') + '\n'
+                with open(e, 'w') as ufile:
+                    filere.writelines(lines)
 
-        if k >= len(lines):
-            ufile = open(e, 'a')
-            t1 = int(input("How many lines you want to add: "))
-            i = 0
-            if i < t1 :           
-                txt = input("the desired text you want to append : ") + '\n'
-                ufile.write(txt)
-                ufile.close()
-            file = open(e, 'r')
-            print(file.read())
+            elif linenum > len(lines):
 
-        elif k < len(lines):
-            txt = input('The new text you want at desired line: ')
-            lines[k] = txt + '\n'
-            ufile = open(e, 'w')
-            ufile.writelines(lines)
-            ufile.close()
-            file = open(e, 'r')
-            print(file.read())
-    else:
-    	print("wrong password")
+                with open(e, 'a') as ufile:
+                    t1 = int(input("How many lines do you want to add? "))
+                    for _ in range(t1):
+                        txt = input("Enter the text you want to append: ") + '\n'
+                        ufile.write(txt)
+                print("New lines added successfully.")
+
+            else:
+                print("Invalid line number.")
+        
+        else:
+            print("Wrong password.")
+    except FileNotFoundError:
+        print("File not found.")
+else:
+    print("Only R and U are valid inputs. Restart the program to continue.")
